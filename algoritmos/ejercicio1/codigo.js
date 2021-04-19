f26638332283f6196d9922aa8c2d266c0143d5eb
justnanna <arianna.espitia1312@gmail.com>
2021-04-19 12:54:06 -0500

    first commit

---
+++ algoritmos/ejercicio1/codigo.js
@@ -0,0 +1,6 @@
+function multiplicar(){
+  var v1 = parseInt(document.getElementById("n1").value);
+  var v2 = parseInt(document.getElementById("n2").value);
+  document.getElementById("respuesta").innerHTML = "<font color=blue>" + (v1 * v2); + "</font>"
+
+} 
---
+++ algoritmos/ejercicio2/codigo.js
@@ -0,0 +1,7 @@
+
+function multiplicar(){
+    var v1 = parseInt(document.getElementById("n1").value);
+   
+    document.getElementById("respuesta").innerHTML = "<font color=blue>" + (3.14) *(v1 * v1); + "</font>"
+  
+  } 
